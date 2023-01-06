0ff47f91ec32c061023a6c28dad733cef68a8f5f
joseg-alvesg <cinhoalves85@gmail.com>
2023-01-06 19:44:27 -0300

    finalização da customização e da HOME e inicio da criação do pages/newproject e do componente projectForm

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import styles from './NewProject.module.css';
 
 export default function NewProject() {
   return (
-    <h1>NewProject</h1>
+    <div className={ styles.newproject_container }>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <p>formulário</p>
+    </div>
   )
 }
---
+++ src/components/projects/ProjectFrom.jsx
@@ -0,0 +1,7 @@
+import styles from './ProjectFrom.module.css';
+
+export default function ProjectFrom() {
+  return (
+    <div>ProjectFrom</div>
+  )
+}
