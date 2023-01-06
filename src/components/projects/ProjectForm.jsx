7c0d0bcc0170248f5876f93e16f72bb4b04c13aa
joseg-alvesg <cinhoalves85@gmail.com>
2023-01-06 20:27:18 -0300

    componentização dos inputs e estilização

--- src/components/projects/ProjectForm.jsx
+++ src/components/projects/ProjectForm.jsx
@@ -1,14 +1,21 @@
+import Input from '../form/Input';
 import styles from './ProjectForm.module.css';
 
 export default function ProjectForm() {
   return (
-    <form>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto" />
-      </div>
-      <div>
-        <input type="number" placeholder="Insira o orçamento toral" />
-      </div>
+    <form className={ styles.form }>
+      <Input 
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o orçamento toral"
+      />
       <div>
         <select name="category_id" id="">
           <option disabled>Seleciona a categoria</option>
