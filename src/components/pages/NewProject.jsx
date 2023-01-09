0e4bcc7362d1185af416a4863784550a2bbec6a1
joseg-alvesg <cinhoalves85@gmail.com>
2023-01-09 13:31:55 -0300

    mensagens de sucesso e erro durante a criação de novos projetos componentizado

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,12 +1,35 @@
+import { useHistory } from 'react-router-dom';
 import ProjectForm from '../projects/ProjectForm';
 import styles from './NewProject.module.css';
 
 export default function NewProject() {
+
+  const history = useHistory();
+
+  function creatPost(project) {
+    //initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/categories", {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    }).then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        // redirect
+        history.push('/projects', {message: 'Projeto criado com sucesso!'})
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className={ styles.newproject_container }>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar Projeto" />
+      <ProjectForm handleSubmit={ creatPost } btnText="Criar Projeto" />
     </div>
   )
 }
